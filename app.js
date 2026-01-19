9b06dc42ea463c83d5ca2c092b506584c9e6bae7
kaneezfatima428 <hafizakaneezfatima.04@gmail.com>
2026-01-19 15:34:49 +0500

    practice

---
+++ app.js
@@ -0,0 +1,20 @@
+//  var text = "loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum voluptatibus debitis nemo praesentium commodi dicta animi? Nesciunt, odit dolor? Nostrum, vero. Molestiae commodi, possimus omnis aliquid officia dolore accusamus earum."
+//  console.log(text.indexOf("dolor"));
+ 
+// if(text.indexOf("Dolorum") !== -1){
+//     console.log( "word found");
+// } else {
+//     console.log("word not found");
+// }
+// if(text.indexOf("world") !== -1){
+//     console.log( "word found");
+// } else {
+//     console.log("word not found");
+// }
+
+// var myname = "kaneezfatima"
+// var firstchar = myname.slice(0,1)
+// var remainchar = myname.slice(1)
+// var titlechar = firstchar.toUpperCase() + remainchar.toLowerCase()
+// console.log(titlechar);
+
